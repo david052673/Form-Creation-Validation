7cfac9aa8fb4029cc6a3bd8a4fd58b9b2b396c1e
David <davidmwathi412@gmail.com>
2024-07-21 16:48:36 +0300

    Added changes

---
+++ script.js
@@ -0,0 +1,43 @@
+document. addEventListener("DOMContentLoaded", (Event)=>{
+       parsed
+});
+
+const form = document. getElementById(id="registration-form");
+console.log (form);
+
+const feedbackDiv = document. querySelector(id="form-feedback");
+
+
+submit. addEventListener(
+     Event.preventDefault() );
+
+
+    let .value= document.getElementById(id="username");
+
+
+    let .value= document.getElementById(id="email");
+
+    let .value= document.getElementById(id="password");
+
+    let isValid="true"
+    let messages= ["error","incorrect","input a strong password"]
+
+
+    ["if (username<3)",isValid=false]{
+       ["message[0]"]
+    }["else"]{
+        [""]
+    }
+
+
+    ["if (email==="@$.")",isValid=true]{
+       [""]
+    }["else",isValid=false]{
+       ["message[1]"]
+    }
+
+    ["if (password==<8)",isValid=false]{
+       ["message[2]"]
+    }["else"]{
+        [""]
+    }
